0dd4a1800e317977b18ee3f00cf3d3c4b65d9b57
Alexander <alex.bukin@hotmail.com>
2025-02-21 20:31:40 +0000

    Connect api to the express app

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express';
 import http from 'http';
 
 import connectToMongoDB from './db';
+import api from './routes/api';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use('/v1', api);
 
 connectToMongoDB();
 
