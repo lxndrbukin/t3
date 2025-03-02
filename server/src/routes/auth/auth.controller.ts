f512a5aede4f979abf5e05ab7d5d23bd9553de90
Alexander <alex.bukin@hotmail.com>
2025-03-02 18:32:17 +0000

    Refactor auth logic to move the passport login functionality from index.ts to routes/auth/auth.router.ts

--- server/src/routes/auth/auth.controller.ts
+++ server/src/routes/auth/auth.controller.ts
@@ -1,19 +1,7 @@
 import { Request, Response } from 'express';
 
-import User from '../../models/user.model';
-
 export const login = (req: Request, res: Response) => {
   res.redirect('/');
-};
-
-export const signup = async (req: Request, res: Response) => {
-  const { displayName, email } = req.session as any;
-  const id = (await User.countDocuments()) + 1;
-  const user = new User({ id, name, email });
-  await user.save();
-  req.login(user, () => {
-    res.redirect('/');
-  });
 };
 
 export const logout = (req: Request, res: Response) => {
