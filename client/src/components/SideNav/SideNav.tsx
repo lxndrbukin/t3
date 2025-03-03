eb19c7817f96bbcc47f377a99098d429ee4f5aaa
Alexander <alex.bukin@hotmail.com>
2025-03-03 21:02:38 +0000

    Add initial structure and  styling to SideNav component

--- client/src/components/SideNav/SideNav.tsx
+++ client/src/components/SideNav/SideNav.tsx
@@ -4,17 +4,36 @@ export default function Nav() {
   return (
     <div className='nav'>
       <div className='nav-logo'>T3</div>
-      <ul className='nav-menu'>
-        <li>
-          <a href='/dashboard'>Dashboard</a>
-        </li>
-        <li>
-          <a href='/tasks'>Tasks</a>
-        </li>
-        <li>
-          <a href='/login'>Login</a>
-        </li>
-      </ul>
+      <div className='nav-menu-container'>
+        <ul className='nav-menu'>
+          <li>
+            <a href='/dashboard'>
+              <div className='nav-icon'></div>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href='/tasks'>
+              <div className='nav-icon'></div>
+              <span>Tasks</span>
+            </a>
+          </li>
+          <li>
+            <a href='/login'>
+              <div className='nav-icon'></div>
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
+        <ul className='nav-submenu'>
+          <li>
+            <a href='/settings'>
+              <div className='nav-icon'></div>
+              <span>Settings</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
