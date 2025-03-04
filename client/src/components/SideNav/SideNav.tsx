d0e0d6f884ff64f87a85ba2b10e5b4d75ee7d6b1
Alexander <alex.bukin@hotmail.com>
2025-03-04 19:22:45 +0000

    Refactor link rendering on the SideNav with renderLinks function and extracting the links data into arrays.

--- client/src/components/SideNav/SideNav.tsx
+++ client/src/components/SideNav/SideNav.tsx
@@ -1,38 +1,29 @@
 import './assets/styles.scss';
 
-export default function Nav() {
+import { NavLink } from './types';
+
+import { navLinks, subNavLinks } from './assets/links';
+
+export default function SideNav() {
+  const renderLinks = (links: Array<NavLink>) => {
+    return links.map(({ name, href, icon }: NavLink) => {
+      return (
+        <li key={name}>
+          <a href={href}>
+            <div className='nav-icon'></div>
+            <span>{name}</span>
+          </a>
+        </li>
+      );
+    });
+  };
+
   return (
     <div className='nav'>
       <div className='nav-logo'>T3</div>
       <div className='nav-menu-container'>
-        <ul className='nav-menu'>
-          <li>
-            <a href='/dashboard'>
-              <div className='nav-icon'></div>
-              <span>Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href='/tasks'>
-              <div className='nav-icon'></div>
-              <span>Tasks</span>
-            </a>
-          </li>
-          <li>
-            <a href='/login'>
-              <div className='nav-icon'></div>
-              <span>Logout</span>
-            </a>
-          </li>
-        </ul>
-        <ul className='nav-submenu'>
-          <li>
-            <a href='/settings'>
-              <div className='nav-icon'></div>
-              <span>Settings</span>
-            </a>
-          </li>
-        </ul>
+        <ul className='nav-menu'>{renderLinks(navLinks)}</ul>
+        <ul className='nav-submenu'>{renderLinks(subNavLinks)}</ul>
       </div>
     </div>
   );
---
+++ client/src/components/SideNav/assets/links.ts
@@ -0,0 +1,13 @@
+import { NavLink } from '../types';
+
+export const navLinks: Array<NavLink> = [
+  { name: 'Dashboard', href: '/dashboard', icon: '' },
+  { name: 'Tasks', href: '/tasks', icon: '' },
+  { name: 'Members', href: '/members', icon: '' },
+];
+
+export const subNavLinks: Array<NavLink> = [
+  { name: 'Settings', href: '/settings', icon: '' },
+  { name: 'Login', href: '/login', icon: '' },
+  { name: 'Logout', href: '/logout', icon: '' },
+];
