79d5c223d9332895888d048e6e26460d4d3ecf91
Alexander <alex.bukin@hotmail.com>
2025-04-04 20:35:54 +0100

    Refactor methods for creating tasks and categories

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -2,12 +2,10 @@ import express from 'express';
 
 import authRouter from './auth/auth.router';
 import tasksRouter from './tasks/tasks.router';
-import categoriesRouter from './tasks/categories.router';
 
 const api = express.Router();
 
 api.use('/auth', authRouter);
 api.use('/tasks', tasksRouter);
-api.use('/categories', categoriesRouter);
 
 export default api;
