fc55694993baa7c01c3a8c9e937949091c68eb1d
Alexander <alex.bukin@hotmail.com>
2025-04-05 19:23:26 +0100

    Create separate file for typescript types in tasks folder

---
+++ server/src/routes/tasks/types/index.ts
@@ -0,0 +1,14 @@
+export enum ItemType {
+  TASK = 'task',
+  CATEGORY = 'category',
+}
+
+export enum ErrorMessage {
+  TASK_NOT_FOUND = 'Task not found',
+  TASK_CATEGORIES_NOT_FOUND = 'Task categories not found',
+  TASKS_LIST_NOT_FOUND = 'Tasks list not found',
+  UNKNOWN_ERROR = 'Unknown error',
+  CATEGORY_NOT_FOUND = 'Category not found',
+  CATEGORY_ALREADY_EXISTS = 'Category already exists',
+  INTERNAL_SERVER_ERROR = 'Internal server error',
+}
