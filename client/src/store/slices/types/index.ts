6064b1972284c6eb2cb3d520b0a0033874ed190a
Alexander <alex.bukin@hotmail.com>
2025-04-16 20:52:39 +0100

    Refactoring client from tasks to boards

--- client/src/store/slices/types/index.ts
+++ client/src/store/slices/types/index.ts
@@ -23,3 +23,28 @@ export type TaskProps = {
   createdAt: string;
   dueDate: string;
 };
+
+export type BoardProps = {
+  id: number;
+  owner: number;
+  boardName: string;
+  columns: {
+    id: number;
+    name: string;
+    order: number;
+    tasks: {
+      id: number;
+      title: string;
+      description: string;
+      category: string;
+      completed: boolean;
+      createdAt: string;
+      dueDate: string;
+    }[];
+  }[];
+};
+
+export type BoardsProps = {
+  list: string[];
+  currentBoard: BoardProps | null;
+};
