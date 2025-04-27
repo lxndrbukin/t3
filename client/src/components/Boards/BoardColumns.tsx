f74e4442cccb85a93986e3bb60f764eada9585c2
Alexander <alex.bukin@hotmail.com>
2025-04-27 15:24:30 +0100

    Working on Create Column to add additional status options

--- client/src/components/Boards/BoardColumns.tsx
+++ client/src/components/Boards/BoardColumns.tsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
 import { BoardColumnProps } from '../../store';
 import BoardColumn from './BoardColumn';
+import Popup from '../../assets/reusable/Popup';
 
 export default function BoardColumns({
   columns,
 }: {
   columns: BoardColumnProps[];
 }) {
+  const [showCreateColumnForm, setShowCreateColumnForm] =
+    useState<boolean>(false);
+
+  const handleCreateColumn = () => {
+    setShowCreateColumnForm(!showCreateColumnForm);
+  };
+
+  const renderCreateColumn = () => {
+    return (
+      <div onClick={handleCreateColumn} className='board-create-column'>
+        <i className='fa-solid fa-plus'></i>
+      </div>
+    );
+  };
+
+  const renderCreateColumnForm = () => {
+    return (
+      <Popup setIsVisible={setShowCreateColumnForm}>
+        <div className='board-create-column-form'>
+          <input placeholder='Column Name' />
+          <button>Create</button>
+        </div>
+      </Popup>
+    );
+  };
+
   const renderColumns = () => {
     if (columns) {
       return columns.map((column) => (
@@ -15,5 +43,10 @@ export default function BoardColumns({
     return null;
   };
 
-  return <div className='board-columns'>{renderColumns()}</div>;
+  return (
+    <div className='board-columns'>
+      {renderColumns()}
+      {renderCreateColumn()}
+    </div>
+  );
 }
