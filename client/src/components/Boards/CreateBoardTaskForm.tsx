de60ce38edea2eb55aa06032d52d9e3a41029de3
Alexander <alex.bukin@hotmail.com>
2025-04-28 19:31:03 +0100

    Working on CreateBoardTaskForm

--- client/src/components/Boards/CreateBoardTaskForm.tsx
+++ client/src/components/Boards/CreateBoardTaskForm.tsx
@@ -1,22 +1,81 @@
-export default function CreateBoardTaskForm() {
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  RootState,
+  AppDispatch,
+  BoardProps,
+  BoardListItemProps,
+  getBoardsList,
+} from '../../store';
+
+type CreateBoardTaskFormProps = {
+  boards?: BoardListItemProps[];
+  currentColumn?: string;
+  currentBoard?: BoardProps;
+};
+
+export default function CreateBoardTaskForm({
+  boards,
+  currentColumn,
+  currentBoard,
+}: CreateBoardTaskFormProps) {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.session);
+
+  useEffect(() => {
+    if (boards?.length === 0) {
+      dispatch(getBoardsList(user?.userId!));
+    }
+  }, [boards?.length]);
+
+  const renderColumnSelectOptions = () => {
+    if (currentBoard) {
+      return currentBoard.columns.map((column) => (
+        <option
+          key={column.id}
+          value={column.name}
+          selected={column.name === currentColumn}
+        >
+          {column.name}
+        </option>
+      ));
+    }
+    return null;
+  };
+
+  const renderBoardSelectOptions = () => {
+    if (boards) {
+      return boards.map((board) => (
+        <option
+          key={board.id}
+          value={board.boardName}
+          selected={board.boardName === currentBoard?.boardName}
+        >
+          {board.boardName}
+        </option>
+      ));
+    }
+    return null;
+  };
+
   return (
     <div className='create-board-task-form'>
       <h2>Create Task</h2>
       <form>
         <div className='form-select'>
           <label htmlFor='board'>Board</label>
-          <select name='board' id='board'>
-            <option value='Board 1'>Board 1</option>
-            <option value='Board 2'>Board 2</option>
-            <option value='Board 3'>Board 3</option>
+          <select
+            name='board'
+            id='board'
+            disabled={currentBoard ? true : false}
+          >
+            {renderBoardSelectOptions()}
           </select>
         </div>
         <div className='form-select'>
           <label htmlFor='type'>Type</label>
           <select name='type' id='type'>
-            <option value='To Do'>To Do</option>
-            <option value='In Progress'>In Progress</option>
-            <option value='Done'>Done</option>
+            {renderColumnSelectOptions()}
           </select>
         </div>
         <div className='form-input'>
