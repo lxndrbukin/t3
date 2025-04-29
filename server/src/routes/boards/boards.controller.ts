05215b56fe1b4b35258723dbf439e490bf4e8656
Alexander <alex.bukin@hotmail.com>
2025-04-29 20:20:10 +0100

    Added functionality to create tasks on each individual column. Fixing board schema to include project key for future unique task keys

--- server/src/routes/boards/boards.controller.ts
+++ server/src/routes/boards/boards.controller.ts
@@ -10,7 +10,7 @@ const asyncHandler =
   };
 
 export const createBoard = async (req: Request, res: Response) => {
-  const { boardName, description, columns } = req.body;
+  const { boardName, description, columns, key } = req.body;
   const boardId = await TasksBoard.countDocuments({
     owner: (req.session as any).passport.user,
   });
@@ -19,6 +19,7 @@ export const createBoard = async (req: Request, res: Response) => {
       id: boardId + 1,
       owner: (req.session as any).passport.user,
       boardName,
+      key,
       description,
       columns: boardColumns(columns),
       members: [
@@ -68,11 +69,13 @@ export const getBoard = asyncHandler(async (req: Request, res: Response) => {
     const filteredBoard = {
       ...tasksBoard,
       columns: tasksBoard.columns.map((col: any) => ({
+        id: col.id,
         name: col.name,
         order: col.order,
         tasks: col.tasks.map((task: any) => ({
           id: task.id,
           title: task.title,
+          description: task.description,
           completed: task.completed,
           dueDate: task.dueDate,
         })),
@@ -150,6 +153,7 @@ export const createColumn = asyncHandler(
         {
           $push: {
             columns: {
+              id: columnOrder + 1,
               name: columnName,
               order: columnOrder,
               tasks: [],
@@ -211,49 +215,33 @@ export const updateColumn = async (req: Request, res: Response) => {
 };
 
 export const createTask = asyncHandler(async (req: Request, res: Response) => {
-  const { columnId, title, dueDate } = req.body;
-  const { boardId } = req.params;
+  const { title, dueDate, description } = req.body;
+  const boardId = Number(req.params.boardId);
+  const columnId = Number(req.params.columnId);
   let currentBoard = await TasksBoard.findOne({
     owner: (req.session as any).passport.user,
     id: boardId,
   });
-
   if (!currentBoard) {
-    return res.status(404).json({ message: 'Board not found' });
+    return res.status(404).json({ message: ErrorMessage.BOARD_NOT_FOUND });
   }
-
-  if (!currentBoard.columns[columnId]) {
-    return res.status(404).json({ message: 'Column not found' });
+  if (!currentBoard.columns[columnId - 1]) {
+    return res.status(404).json({ message: ErrorMessage.COLUMN_NOT_FOUND });
   }
-
-
-  const taskId = currentBoard.columns[columnId].tasks.length + 1;
+  const taskId = currentBoard.columns[columnId - 1].tasks.length + 1;
   try {
-    currentBoard = await TasksBoard.findOneAndUpdate(
-      {
-        owner: (req.session as any).passport.user,
-        id: boardId,
-      },
-      {
-        $push: {
-          [`columns.$[columnIndex].tasks`]: {
-            $each: [
-              {
-                id: taskId,
-                title,
-                dueDate,
-                completed: false,
-              },
-            ],
-            $position: columnId,
-          },
-        },
-      },
-      {
-        arrayFilters: [{ columnIndex: { $eq: columnId } }],
-      }
-    );
-    res.status(204).json({...currentBoard!.columns[columnId].tasks[taskId - 1], columnId});
+    currentBoard.columns[columnId - 1].tasks.push({
+      id: taskId,
+      title,
+      description,
+      dueDate,
+      completed: false,
+    });
+    await currentBoard.save();
+    res.status(204).json({
+      ...currentBoard.columns[columnId - 1].tasks[taskId - 1],
+      columnId,
+    });
   } catch (error) {
     res.status(500).json({
       message:
--- server/src/routes/boards/types.ts
+++ server/src/routes/boards/types.ts
@@ -2,5 +2,10 @@ export enum ErrorMessage {
   UNKNOWN_ERROR = 'An unknown error occurred',
   BOARD_NOT_FOUND = 'Board not found',
   COLUMN_NOT_FOUND = 'Column not found',
-  INVALID_REQUEST = 'Invalid request data'
+  INVALID_REQUEST = 'Invalid request data',
+  TASK_NOT_FOUND = 'Task not found',
+  TASK_CATEGORIES_NOT_FOUND = 'Task categories not found',
+  TASK_CATEGORY_NOT_FOUND = 'Task category not found',
+  TASK_CATEGORY_ALREADY_EXISTS = 'Task category already exists',
+  TASKS_LIST_NOT_FOUND = 'Tasks list not found',
 }
