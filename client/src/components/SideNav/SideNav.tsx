2feee7a867d356044dea000ad0f9a20e336606bb
Alexander <alex.bukin@hotmail.com>
2025-04-30 20:16:06 +0100

    Update auth buttons on SideNav to show depending on the current auth status of the user

--- client/src/components/SideNav/SideNav.tsx
+++ client/src/components/SideNav/SideNav.tsx
@@ -1,15 +1,22 @@
-import './assets/styles.scss';
-import { Link } from 'react-router-dom';
-import { NavLink } from './types';
-import { navLinks, subNavLinks } from './assets/links';
+import "./assets/styles.scss";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { NavLink } from "./types";
+import { navLinks, subNavLinks } from "./assets/links";
+import { RootState } from "../../store";
 
 export default function SideNav() {
+  const { user } = useSelector((state: RootState) => state.session);
+
   const renderLinks = (links: Array<NavLink>) => {
-    return links.map(({ name, href, icon }: NavLink) => {
+    return links.map(({ name, href, icon, auth }: NavLink) => {
+      if ((auth === true && !user) || (auth === false && user)) {
+        return null;
+      }
       return (
         <li key={name}>
           <Link title={name} to={href}>
-            <i className={icon ? icon : 'nav-icon'}></i>
+            <i className={icon ? icon : "nav-icon"}></i>
           </Link>
         </li>
       );
@@ -17,13 +24,13 @@ export default function SideNav() {
   };
 
   return (
-    <div className='nav'>
-      <Link to='/' className='nav-logo'>
-        <i title='Taskify' className='fa-solid fa-check-double'></i>
+    <div className="nav">
+      <Link to="/" className="nav-logo">
+        <i title="Taskify" className="fa-solid fa-check-double"></i>
       </Link>
-      <div className='nav-menu-container'>
-        <ul className='nav-menu'>{renderLinks(navLinks)}</ul>
-        <ul className='nav-submenu'>{renderLinks(subNavLinks)}</ul>
+      <div className="nav-menu-container">
+        <ul className="nav-menu">{renderLinks(navLinks)}</ul>
+        <ul className="nav-submenu">{renderLinks(subNavLinks)}</ul>
       </div>
     </div>
   );
