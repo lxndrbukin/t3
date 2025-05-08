bedbcb9bc78fb293739ae3be06a9f24e829a9b04
Alexander <alex.bukin@hotmail.com>
2025-05-08 20:52:01 +0100

    Working on auth form for local auth

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -1,7 +1,7 @@
-import "./assets/styles.scss";
-import { FormEvent } from "react";
-import { useDispatch } from "react-redux";
-import { AppDispatch, googleLogin } from "../../store";
+import './assets/styles.scss';
+import { FormEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch, googleLogin } from '../../store';
 
 export default function Auth() {
   const dispatch = useDispatch<AppDispatch>();
@@ -11,18 +11,31 @@ export default function Auth() {
     dispatch(googleLogin());
   };
 
+  const renderAuthForm = () => {
+    return (
+      <form>
+        <input type='text' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <button type='submit'>Login</button>
+      </form>
+    );
+  };
+
   return (
-    <div className="auth">
-      <h1>Login</h1>
-      <div className="auth-buttons">
-        <button className="auth-button google" onClick={handleClick}>
-          <i className="fa-brands fa-google"></i>
-          <span>Login with Google</span>
-        </button>
-        <button className="auth-button facebook" onClick={handleClick}>
-          <i className="fa-brands fa-facebook"></i>
-          <span>Login with Facebook</span>
-        </button>
+    <div className='auth-container'>
+      <div className='auth'>
+        <h1>Login</h1>
+        {renderAuthForm()}
+        <div className='auth-buttons'>
+          <button className='auth-button google' onClick={handleClick}>
+            <i className='fa-brands fa-google'></i>
+            <span>Login with Google</span>
+          </button>
+          <button className='auth-button facebook' onClick={handleClick}>
+            <i className='fa-brands fa-facebook'></i>
+            <span>Login with Facebook</span>
+          </button>
+        </div>
       </div>
     </div>
   );
